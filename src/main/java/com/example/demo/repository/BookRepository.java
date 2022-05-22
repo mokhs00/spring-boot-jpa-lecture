1b044df2c55dfe986fe9f9bb1a44585203805c91
mokhs00 <mokhs00@naver.com>
2022-05-22 16:15:26 +0900

    Update use Optional

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -3,11 +3,12 @@ package com.example.demo.repository;
 import com.example.demo.domain.Book;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository {
     List<Book> findAll();
 
-    Book findById(Long id);
+    Optional<Book> findById(Long id);
 
     Book save(Book book);
 
