8de9c59ecc082ea50222e0834dcabeeba25af414
mokhs00 <mokhs00@naver.com>
2022-05-22 16:04:07 +0900

    Add Book model

---
+++ src/main/java/com/example/demo/domain/Book.java
@@ -0,0 +1,30 @@
+package com.example.demo.domain;
+
+public class Book {
+    private Long id;
+    private String title;
+    private Long authorId;
+
+    public Book(Long id, String title, Long authorId) {
+        this.id = id;
+        this.title = title;
+        this.authorId = authorId;
+    }
+
+    public Book(String title, Long authorId) {
+        this.title = title;
+        this.authorId = authorId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+}
