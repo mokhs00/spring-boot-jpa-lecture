12d673ee1ceb9741662a0b2fb88cd3b2a1784001
HanSu Kim <mokhs00@naver.com>
2022-06-04 15:02:28 +0900

    Update to use JPA (#2)
    * Add gradle dependency spring-boot-starter-data-jpa
    
    * Add gradle dependency h2 database
    
    * Add @Entity to Book
    
    * Add BookJpaRepository
    
    * Update BookService to use JpaRepository

--- src/main/java/com/example/demo/domain/Book.java
+++ src/main/java/com/example/demo/domain/Book.java
@@ -1,9 +1,20 @@
 package com.example.demo.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private Long authorId;
+
+    protected Book() {
+    }
 
     public Book(Long id, String title, Long authorId) {
         this.id = id;
